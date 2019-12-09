d2e4ab6f4962adee3daf5668ac9cc14df62ac323
harri <harri@10.2.57.226>
2019-12-09 11:01:35 -0600

    //added a comment to HarrisonsClass

--- src/HarrisonsClass.java
+++ src/HarrisonsClass.java
@@ -4,6 +4,7 @@ public class HarrisonsClass {
 	public static void main( String[] args ) {
 		System.out.println("This is Harrison's class");
 		System.out.println("this is a change");
+		//blah blah blah
 	}
 }
 
